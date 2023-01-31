3338b7eea9a9b753f5613d4a434b34a1485ad460
sohanur28 <gsrahman28@gmail.com>
2023-01-31 13:47:29 +0600

    numbers

--- while.js
+++ while.js
@@ -2,6 +2,13 @@
 
 // }
 
+/**
+ * 1. Loop variable
+ * 2. Condition inside while
+ * 3. Loop body
+ * 4. Change the loop variable
+ * */ 
+
 var roastGiven = 0;
 while (roastGiven < 7) {
     console.log('Roast Den, Please !!');
